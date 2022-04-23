a63eda92b67f99809839538b713f08cfff4cdac9
Satyam Abhishek <satyam.abhi02@gmail.com>
2022-04-23 23:44:47 +0530

    Updates

---
+++ precache-manifest.cab5b89c5f8841d1fdcc507ca4d7fdbd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4babe8d0dc94294e2fc",
+    "url": "/Monsters-rolodex/static/css/main.f8310be3.chunk.css"
+  },
+  {
+    "revision": "c4babe8d0dc94294e2fc",
+    "url": "/Monsters-rolodex/static/js/main.c4babe8d.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/Monsters-rolodex/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "10992e196e3dd19b20c1",
+    "url": "/Monsters-rolodex/static/js/2.10992e19.chunk.js"
+  },
+  {
+    "revision": "2c9c8d1f1a023ab2fa37",
+    "url": "/Monsters-rolodex/static/js/runtime~main.2c9c8d1f.js"
+  },
+  {
+    "revision": "5b89b767cc82c7617de2ba5db7abb091",
+    "url": "/Monsters-rolodex/index.html"
+  }
+];
